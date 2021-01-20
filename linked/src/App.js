a89cedb549b9a7c7801e59a9f894215e80c963cd
Himanshu Bainwala <hbainwala@gmail.com>
2021-01-19 23:34:49 -0800

    Error checking for nothing is playing

--- linked/src/App.js
+++ linked/src/App.js
@@ -8,13 +8,14 @@ function App() {
 
   const spotifyWebAPI = new Spotify();
   const [nowPlaying, setNowPlaying] = useState({name: 'Not Checked', image: ''});
+  const [logged, setLogged] = useState(false);
 
   useEffect(() => {
     const params = getHashParams();
     console.log(spotifyWebAPI)
     if(params.access_token) {
-      console.log('Logged In')
-      spotifyWebAPI.setAccessToken(params.access_token)
+      setLogged(true);
+      spotifyWebAPI.setAccessToken(params.access_token);
     }
   },[])
   
@@ -31,18 +32,34 @@ function App() {
 
   function Playing() { 
     spotifyWebAPI.getMyCurrentPlaybackState()
-    .then(res => setNowPlaying({name:res.item.name, image: res.item.album.images[0].url}))
+    .then(res => {
+      if(res) {
+        setNowPlaying({name:res.item.name, image: res.item.album.images[0].url})
+      } else {
+        setNowPlaying({name:'Nothing Is Playing', image: ''})
+      }
+    })
+   }
+
+   function Content() {
+     if(!logged) {
+       return <Spottify/>
+     } else {
+       return (
+        <div>
+          <div>Now Playing: {nowPlaying.name}</div>
+          <div>
+            <img src={nowPlaying.image} style={{width:100}}/>
+          </div>
+          <button onClick = {Playing}>Check Now Playing</button>
+       </div>
+       )
+     }
+
    }
 
   return (
-    <div>
-      <Spottify/>
-      <div>Now Playing: {nowPlaying.name}</div>
-      <div>
-        <img src={nowPlaying.image} style={{width:100}}/>
-      </div>
-      <button onClick = {Playing}>Check Now Playing</button>
-    </div>
+    <Content/>
   );
 }
 
